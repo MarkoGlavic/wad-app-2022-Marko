4f35235cc45bf703e3ddd72004bd5e04781bfa5d
Marko Glavic <glavim@gmail.com>
2022-11-20 19:08:29 +0000

    Mongo lab exercises

--- node-lab1/movies-api/api/genres/index.js
+++ node-lab1/movies-api/api/genres/index.js
@@ -1,23 +1,14 @@
 import express from 'express';
-import { genres } from './genresData';
+import Genres from './genresModel';
+const router = express.Router();
 
-const router = express.Router(); 
-router.get('/', (req, res) => {
-    res.json(genres);
+router.get('/', async (req, res) => {
+    const genres = await Genres.find();
+    res.status(200).json(genres);
 });
 
 
-router.get('/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    if (genres.id == id) {
-        res.status(200).json(genres);
-    } else {
-        res.status(404).json({
-            message: 'The resource you requested could not be found.',
-            status_code: 404
-        });
-    }
-});
+
 
 
 export default router;
--- node-lab1/movies-api/seedData/index.js
+++ node-lab1/movies-api/seedData/index.js
@@ -1,10 +1,15 @@
 import userModel from '../api/users/userModel';
+import genresModel from '../api/genres/genresModel'
 import users from './users';
 import dotenv from 'dotenv';
+import genres from './genres';
 
 dotenv.config();
 
 // deletes all user documents in collection and inserts test data
+
+
+
 async function loadUsers() {
   console.log('load user Data');
   try {
@@ -16,6 +21,19 @@ async function loadUsers() {
   }
 }
 
+async function loadGenres() {
+    console.log('load genres');
+    try {
+      await genresModel.deleteMany();
+      await genresModel.collection.insertMany(genres);
+      console.info(`${genres.length} genres were successfully stored.`);
+    } catch (err) {
+      console.error(`failed to Load genres: ${err}`);
+    }
+  }
+
+
 if (process.env.SEED_DB) {
   loadUsers();
+  loadGenres();
 }
